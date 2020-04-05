315a52ec06182e4138cd05b52da9916d311ffa62
beldimouheb <mouhebbeldi@gmail.com>
2020-04-05 04:04:59 +0100

    deploy

--- server.js
+++ server.js
@@ -23,11 +23,11 @@ app.use('/api/test',require('./routes/test'));
 app.use('/api/auth',require('./routes/auth'));
 
 // serve static assets in production 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build'));
-    app.get('*',(req, res )=>
-        res.sendFile(path.resolve(__dirname,'client', 'build', 'index.html')));
-}
+// if(process.env.NODE_ENV === 'production'){
+//     app.use(express.static('client/build'));
+//     app.get('*',(req, res )=>
+//         res.sendFile(path.resolve(__dirname,'client', 'build', 'index.html')));
+// }
 
 
 app.listen(PORT,() =>{
