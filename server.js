df4d019803e6025ed262d003fbe6d488ff97d356
beldimouheb <mouhebbeldi@gmail.com>
2020-04-05 03:20:27 +0100

    prepare for deploy

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express =require('express');
 const connectDB = require('./config/db');
 const cors = require('cors');
 const app=express();
+const path = require('path');
 app.use(cors());
 
 
@@ -21,7 +22,12 @@ app.use('/api/users',require('./routes/users'));
 app.use('/api/test',require('./routes/test'));
 app.use('/api/auth',require('./routes/auth'));
 
-
+// serve static assets in production 
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+    app.get('*',(req, res )=>
+        res.sendFile(path.resolve(__dirname,'client', 'build', 'index.html')));
+}
 
 
 app.listen(PORT,() =>{
